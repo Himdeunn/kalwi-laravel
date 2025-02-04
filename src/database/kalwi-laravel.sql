0f833d7a3939fa7154137aff92d4ae359dca6edf
ThePeaces <tegarmascart@gmail.com>
2025-02-04 21:08:48 +0700

    update for file sql database

--- src/database/kalwi-laravel.sql
+++ src/database/kalwi-laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 02, 2025 at 01:24 PM
+-- Generation Time: Feb 04, 2025 at 02:07 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `kalwi-laravel`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`, `created_at`) VALUES
+(1, 1, 7, 1, '2025-02-04 14:05:20');
 
 -- --------------------------------------------------------
 
@@ -41,15 +62,23 @@ CREATE TABLE `categories` (
 
 INSERT INTO `categories` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
 (1, 'Ranks', 'ranks', '2025-01-29 07:01:50', '2025-01-29 07:01:50'),
-(2, 'Package', 'package', '2025-01-29 13:37:41', '2025-01-29 13:40:24'),
-(3, 'Membership', 'membership', '2025-01-29 13:37:41', '2025-01-29 13:40:24'),
-(4, 'Subsciption', 'subscription', '2025-01-29 13:40:09', '2025-01-29 13:40:09'),
 (5, 'Perks', 'perks', '2025-01-30 02:29:55', '2025-01-30 02:29:55'),
-(6, 'Kits', 'kits', '2025-01-30 02:43:46', '2025-01-30 02:43:46'),
-(7, 'Role', 'role', '2025-01-30 02:45:01', '2025-01-30 02:45:01'),
-(8, 'Armor', 'armor', '2025-01-30 02:48:35', '2025-01-30 02:48:35'),
-(9, 'Game', 'game', '2025-01-30 03:16:21', '2025-01-30 03:16:21'),
-(10, 'Tes', 'tes', '2025-02-01 07:11:23', '2025-02-01 07:11:23');
+(11, 'Rank Weekly', 'rank_weekly', '2025-02-04 04:52:33', '2025-02-04 04:52:33'),
+(12, 'Rank Monthly', 'rank_monthly', '2025-02-04 04:52:47', '2025-02-04 04:52:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkout`
+--
+
+CREATE TABLE `checkout` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `total_price` decimal(10,2) NOT NULL,
+  `status` enum('pending','completed','cancelled') DEFAULT 'pending',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -78,6 +107,23 @@ INSERT INTO `code_referral` (`id`, `code_number`, `discount_percentage`, `create
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `history_buying` varchar(255) NOT NULL,
+  `real_price` decimal(10,2) NOT NULL,
+  `discount_price` decimal(10,2) NOT NULL,
+  `buying_at` datetime NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
@@ -90,16 +136,19 @@ CREATE TABLE `product` (
   `icon_product` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `update_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `discount_price_product` decimal(10,2) NOT NULL DEFAULT '0.00'
+  `discount_price_product` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `command_product` text NOT NULL,
+  `filter` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `category_id`, `name_product`, `real_price_product`, `description_product`, `icon_product`, `created_at`, `update_at`, `discount_price_product`) VALUES
-(1, 3, 'Elite', 5464.00, 'testingkajnej', 'next (2).png', '2025-02-01 16:47:16', '2025-02-02 11:48:08', 2351.00),
-(4, 8, 'Obsidian', 531261.00, 'asfaeveeva', 'nextjs-light.png', '2025-02-02 11:42:23', '2025-02-02 11:42:23', 135325.00);
+INSERT INTO `product` (`id`, `category_id`, `name_product`, `real_price_product`, `description_product`, `icon_product`, `created_at`, `update_at`, `discount_price_product`, `command_product`, `filter`) VALUES
+(5, 1, 'Ametyst', 2351351.00, 'afegagaegaeve', 'Immortal_3_Rank.png', '2025-02-03 02:04:38', '2025-02-04 12:09:57', 1213140.00, '/lp user <user> promote ametyst', 'ranks'),
+(6, 5, '1000 Coins', 1351353.00, 'afeafaeveascaeea', 'gleaming-gold-coin-wealth-prosperity-fortune.png', '2025-02-04 11:38:11', '2025-02-04 11:40:06', 123513.00, '/balance add <user> 1000', 'perks'),
+(7, 12, 'Iron', 234563.00, 'gctxcrtxdrttxrdf', 'Silver_3_Rank.png', '2025-02-04 12:10:35', '2025-02-04 12:10:35', 14322.00, '/lp user <user> promote iron', 'rank_monthly');
 
 -- --------------------------------------------------------
 
@@ -127,11 +176,19 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `purchase_product`, `wallet`, `email`, `password`, `type`, `created_at`, `updated_at`) VALUES
 (1, 'Admin', 'Tegar', 'admin', NULL, 0.00, 'admin@example.com', '$2y$10$3B9YH13KYeaNN4VMfLzR2.TKW1YMawfcMV2iKljOe7ysKY/ODL79W', 3, '2025-01-28 07:49:33', '2025-02-02 10:50:23'),
-(2, 'John', 'Doe', 'JD', NULL, 0.00, 'user@example.com', '$2y$10$6ob12E8dx5OsVlzOY.R35.9OEsPwxqJWlbbbodu4YmCW6ZFe8282i', 1, '2025-01-28 07:49:33', '2025-01-28 07:52:53');
+(2, 'John', 'Doe', 'JD', NULL, 0.00, 'user@example.com', '$2y$10$ZJITZQIOfOVQF7JAMQrLx.24a9vEQ3FQ8KcDTIA7DBQ1gT/jRG.O2', 1, '2025-01-28 07:49:33', '2025-02-03 00:35:55');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `categories`
@@ -142,12 +199,26 @@ ALTER TABLE `categories`
   ADD UNIQUE KEY `slug` (`slug`);
 
 --
+-- Indexes for table `checkout`
+--
+ALTER TABLE `checkout`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `code_referral`
 --
 ALTER TABLE `code_referral`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `code_number` (`code_number`),
   ADD KEY `created_by` (`created_by`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `product`
@@ -169,10 +240,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `checkout`
+--
+ALTER TABLE `checkout`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `code_referral`
@@ -181,10 +264,16 @@ ALTER TABLE `code_referral`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -197,10 +286,29 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `checkout`
+--
+ALTER TABLE `checkout`
+  ADD CONSTRAINT `checkout_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `code_referral`
 --
 ALTER TABLE `code_referral`
   ADD CONSTRAINT `code_referral_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `history`
+--
+ALTER TABLE `history`
+  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `product`
